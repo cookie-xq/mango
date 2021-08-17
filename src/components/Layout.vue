59ffde910cf6aadec1f6d1b6fb7c79e6c72ac990
fancy <1186710960@qq.com>
2021-08-18 01:08:02 +0800

    HTML 和 CSS 写好了

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      <slot></slot>
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`   ">
+      <slot/>
     </div>
-    <Nav />
+    <Nav/>
   </div>
 </template>
 
+<script lang="ts">
+  export default {
+    props: ['classPrefix'],
+    name: 'Layout'
+  };
+</script>
+
 <style lang="scss" scoped>
-.nav-wrapper {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-.content {
-  flex-grow: 1;
-  overflow: auto;
-}
+  .layout-wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content {
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
