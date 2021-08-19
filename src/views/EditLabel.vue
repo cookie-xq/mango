dde005a82e5169e4a2e60d737ed813a25e9a85a1
fancy <1186710960@qq.com>
2021-08-19 16:51:08 +0800

    封装 tagList 的增删改查

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import tagListModel from '@/models/tagListModel'
+/* global Tag */
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import FormItem from '@/components/Money/FormItem.vue'
@@ -24,27 +24,22 @@ import Button from '@/components/Button.vue'
 
 @Component({ components: { FormItem, Button } })
 export default class EditLabel extends Vue {
-  tag?: { id:string,name:string } = undefined;
+  tag?: Tag = undefined;
   created() {
-    const id = this.$route.params.id
-    tagListModel.fetch()
-    const tags = tagListModel.data
-    const tag = tags.filter((t) => t.id === id)[0]
-    if (tag) {
-      this.tag = tag;
-    } else {
+    this.tag = window.findTag(this.$route.params.id);
+    if (!this.tag) {
       this.$router.replace('/404')
     }
   }
   update(name:string){
     if(this.tag){
-      tagListModel.update(this.tag.id,name);
+      window.updateTag(this.tag.id, name);
     }
     
   }
   remove(){
     if(this.tag){
-      if(tagListModel.remove(this.tag.id)){
+      if(window.removeTag(this.tag.id)){
         this.$router.back();
       }else{
         window.alert('删除失败');
