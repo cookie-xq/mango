7f46e06499de1c088238e57e54f8545b0bdfdec6
fancy <1186710960@qq.com>
2021-08-19 23:13:34 +0800

    latest

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"/>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name" @update:value="update" 
+      <FormItem :value="currentTag.name" @update:value="update" 
                 fileName="标签名" placehoder="请输入标签名" />
     </div>
     <div class="button-wrapper">
@@ -16,7 +16,6 @@
 </template>
 
 <script lang="ts">
-/* global Tag */
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import FormItem from '@/components/Money/FormItem.vue'
@@ -24,33 +23,30 @@ import Button from '@/components/Button.vue'
 
 @Component({ components: { FormItem, Button } })
 export default class EditLabel extends Vue {
-  get tag() {
+  get currentTag() {
     return this.$store.state.currentTag;
   }
   
   created() {
     const id = this.$route.params.id;
-    console.log(id);
     this.$store.commit('fetchTags');
     this.$store.commit('setCurrentTag', id);
-    if (!this.tag) {
-      console.log('no tag');
+    if (!this.currentTag) {
       this.$router.replace('/404')
-    }else {
-      console.log('has tag');
     }
   }
+
   update(name: string){
-    if(this.tag){
+    if(this.currentTag){
       this.$store.commit('updateTag', {
-        id: this.tag.id, name
+        id: this.currentTag.id, name
       });
     }
     
   }
   remove(){
-    if(this.tag){
-      this.$store.commit('removeTag', this.tag.id);
+    if(this.currentTag){
+      this.$store.commit('removeTag', this.currentTag.id);
     }
   }
   goBack(){
