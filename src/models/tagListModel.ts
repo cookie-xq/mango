bfb89342643e2a283454988daf0cc62a6c6bc7fa
fancy <1186710960@qq.com>
2021-08-19 11:18:55 +0800

    将 tag 的 string 改为 {id，name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,24 +1,31 @@
 const localStorageKeyName = 'tagList'
+type Tag = {
+  id: string
+  name: string
+}
 type TagListModel = {
-  data: string[]
-  fetch: () => string[]
+  data: Tag[]
+  fetch: () => Tag[]
   create: (name: string) => 'success' | 'duplicated' // 联合类型
   save: () => void
 }
-const tagListModel : TagListModel= {
-  data:[],
+const tagListModel: TagListModel = {
+  data: [],
   fetch() {
-    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
-    return this.data;
+    this.data = JSON.parse(
+      window.localStorage.getItem(localStorageKeyName) || '[]'
+    )
+    return this.data
   },
   create(name: string) {
-    if(this.data.indexOf(name)>=0){return 'duplicated'}
-    this.data.push(name);
-    this.save();
-    return 'success';
+    const names = this.data.map((item) => item.name)
+    if (names.indexOf(name) >= 0) {return 'duplicated'}
+    this.data.push({ id: name, name: name })
+    this.save()
+    return 'success'
   },
   save() {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data))
   },
 }
 
