f3f45f69d076bd1bcf56b6ca011573ec00405278
fancy <1186710960@qq.com>
2021-08-19 16:41:55 +0800

    使用全局 tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,20 @@ type RecordItem = {
     amount: number 
     createdAt?: Date //除了写数据类型之外还可以写一个类（构造函数），类是对 object 的分类
 }
+
+type Tag = {
+  id: string;
+  name: string;
+}
+type TagListModel = {
+  data: Tag[]
+  fetch: () => Tag[]
+  create: (name: string) => 'success' | 'duplicated' // 联合类型
+  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+  remove: (id: string) => boolean
+  save: () => void
+}
+
+interface Window {
+  tagList: Tag[]
+}
