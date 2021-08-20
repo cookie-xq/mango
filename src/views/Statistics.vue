3bce8166d04e56ffeb3e1cbc41cc5a9fadbe11e4
fancy <1186710960@qq.com>
2021-08-21 01:52:16 +0800

    完成统计功能

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,17 +1,14 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="typeList" :value.sync="type" />
-    <Tabs
-      class-prefix="interval"
-      :data-source="intervalList"
-      :value.sync="interval"
-    />
     <ol>
-      <li v-for="(group, index) in result" :key="index">
-        <h3 class="title">{{ group.title }}</h3>
+      <li v-for="(group, index) in groupedList" :key="index">
+        <h3 class="title">
+          {{ beautify(group.title) }} <span>￥{{ group.total }}</span>
+        </h3>
         <ol>
           <li v-for="item in group.items" :key="item.id" class="record">
-            <span>{{tagString(item.tags)}}</span>
+            <span>{{ tagString(item.tags) }}</span>
             <span class="notes">{{ item.notes }}</span>
             <span>￥{{ item.amount }} </span>
           </li>
@@ -29,45 +26,88 @@ import { Component } from 'vue-property-decorator'
 import Tabs from '@/components/Tabs.vue'
 import intervalList from '@/constants/intervalList'
 import recordTypeList from '@/constants/recordTypeList'
+import dayjs from 'dayjs'
+import clone from '@/lib/clone'
 
 @Component({
   components: { Tabs },
 })
 export default class Statistics extends Vue {
   /* eslint-disable no-undef */
+  beautify(string: string) {
+    const day = dayjs(string)
+    const now = dayjs()
+    if (day.isSame(now, 'day')) {
+      return '今天'
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      console.log('hi')
+      return '昨天'
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天'
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日')
+    } else {
+      return day.format('YYYY年M月D日')
+    }
+  }
   tagString(tags: Tag[]) {
-    
-    if(tags.length === 0){
+    if (tags.length === 0) {
       return '无'
-    }else{
-      let arr = []
-      for(let i = 0; i<tags.length; i++){
+    } else {
+      const arr = []
+      for (let i = 0; i < tags.length; i++) {
         arr.push(tags[i].name)
       }
-      return arr.join(',')
+      return arr.join('、')
     }
   }
   get recordList() {
     return (this.$store.state as RootState).recordList
   }
-  get result() {
+  get groupedList() {
     const { recordList } = this
-    type HashTableValue = { title: string; items: RecordList[] }
-    const hashTable: { [key: string]: HashTableValue } = {}
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createdAt!.split('T')
-      hashTable[date] = hashTable[date] || { title: date, items: [] }
-      hashTable[date].items.push(recordList[i])
+    if (recordList.length === 0) {
+      return []
     }
-    return hashTable
+    const newList = clone(recordList)
+      .filter((r) => r.type === this.type)
+      .sort(
+        (a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf()
+      )
+    if(newList.length === 0){return [] as Result;}
+    type Result = { title: string; total?: number; items: RecordItem[] }[]
+    const result: Result = [
+      {
+        title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'),
+        items: [newList[0]],
+      },
+    ]
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i]
+      const last = result[result.length - 1]
+      if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+        last.items.push(current)
+      } else {
+        result.push({
+          title: dayjs(current.createdAt).format('YYYY-MM-DD'),
+          items: [current],
+        })
+      }
+    }
+    result.map((group) => {
+      group.total = group.items.reduce((sum, item) => {
+        console.log(sum);
+        console.log(item);
+        return sum + parseFloat(item.amount)
+      }, 0)
+    })
+    return result
   }
   beforeCreate() {
     this.$store.commit('fetchRecords')
   }
 
   type = '-'
-  interval = 'day'
-  intervalList = intervalList
   typeList = recordTypeList
 }
 </script>
